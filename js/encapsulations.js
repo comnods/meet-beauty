fd9b448f798321b7b8324fce5b7f314db56c6298
comnods <admin@b8bb.cn>
2019-08-17 21:44:06 +0800

    update

---
+++ js/encapsulations.js
@@ -0,0 +1,10 @@
+/*自己封装的一些比较小的一些方法*/
+$.fn.extend({
+    // 设置元素高宽
+    genWidthAndHeight: function (width, height) {
+        this.css({
+            width: width,
+            height: height
+        });
+    }
+});
